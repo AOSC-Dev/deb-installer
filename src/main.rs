0b1f9373b7bd499207bd631dddf28478341f4abd
eatradish <sakiiily@aosc.io>
2024-11-09 16:05:33 +0800

    feat: add progress bar

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@ use std::{
     path::PathBuf,
     process::{exit, Command},
     sync::atomic::Ordering,
-    thread,
+    thread::{self, JoinHandle},
     time::Duration,
 };
 
@@ -68,7 +68,10 @@ fn main() {
         }
 
         if package.extension().map(|x| x.to_string_lossy()) != Some("deb".into()) {
-            eprintln!("Usage: deb-installer foo.deb");
+            eprintln!(
+                "Usage: {} /path/to/foo.deb",
+                current_exe().unwrap().display()
+            );
             exit(1);
         }
 
@@ -110,12 +113,20 @@ fn ui(pkg: PathBuf) {
 
     let argc = arg.to_string();
 
-    // TODO: Progress bar
-    installer.on_install(move || match on_install(argc.clone()) {
-        Ok(_) => {}
-        Err(e) => {
-            eprintln!("{}", e);
-        }
+    let ui_weak = installer.as_weak();
+
+    let (tx, rx) = flume::unbounded();
+
+    installer.on_install(move || {
+        let t = on_install(argc.clone(), tx.clone());
+    });
+
+    thread::spawn(move || loop {
+        let Ok(progress) = rx.recv() else {
+            break;
+        };
+
+        let _ = ui_weak.upgrade_in_event_loop(move |ui| ui.set_progress(progress as f32 / 100.0));
     });
 
     installer.run().unwrap();
@@ -138,18 +149,22 @@ fn get_package_info(arg: &str) -> Result<PackageInfo> {
     Ok(info)
 }
 
-fn on_install(argc: String) -> Result<()> {
-    let _ = start_backend();
+fn on_install(argc: String, tx: flume::Sender<u32>) -> JoinHandle<Result<()>> {
+    let t = thread::spawn(move || {
+        let _ = start_backend();
 
-    let rt = tokio::runtime::Builder::new_multi_thread()
-        .enable_all()
-        .build()
-        .unwrap();
+        let rt = tokio::runtime::Builder::new_multi_thread()
+            .enable_all()
+            .build()
+            .unwrap();
 
-    rt.block_on(on_install_inner(argc))
+        rt.block_on(on_install_inner(argc, tx))
+    });
+
+    t
 }
 
-async fn on_install_inner(argc: String) -> Result<()> {
+async fn on_install_inner(argc: String, tx: flume::Sender<u32>) -> Result<()> {
     let conn = Connection::system().await?;
     let client = OmaClientProxy::new(&conn).await?;
 
@@ -167,11 +182,14 @@ async fn on_install_inner(argc: String) -> Result<()> {
     loop {
         let is_finished = client.is_finished().await?;
         let progress = get_progress(&client).await?;
+
+        tx.send_async(progress).await?;
+
         if progress == 100 || is_finished {
             client.exit().await?;
             return Ok(());
         }
-        println!("{}", progress);
+
         thread::sleep(Duration::from_millis(100));
     }
 }
